20456e59881341c5fd6a7269c62b3b11bb5814f3
julogav <julietgavison@gmail.com>
2021-10-19 18:05:18 +0300

    content from API added with click events

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,14 @@ import axios from 'axios';
 
 const Main = () => {
 	const baseURL = 'https://swapi.dev/api/films/';
-	const [titles, setTitles] = useState([]);
+	const [data, setData] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [title, setTitle] = useState();
+	const [director, setDirector] = useState();
+	const [date, setDate] = useState();
+	const [abstract, setAbstract] = useState();
+	const loadingMsg = 'Stars are far, give it a sec please...';
 
 	useEffect(() => {
 		getData();
@@ -16,7 +21,7 @@ const Main = () => {
 		await axios
 			.get(baseURL)
 			.then(response => {
-				setTitles(response.data.results);
+				setData(response.data.results);
 			})
 			.catch(error => {
 				console.error('error getting data', error);
@@ -26,20 +31,39 @@ const Main = () => {
 				setLoading(false);
 			});
 	};
-	if (loading) return 'Stars are far, give it a sec please...';
+	if (loading) return loadingMsg;
 	if (error) return 'Oops! a wookiee got in the way...';
 
 	return (
 		<div className='main'>
 			<div className='navbar'>
 				<span className='navTitles'>MOVIES</span>
-				{titles.map(movie => (
-					<p key={movie.episode_id}>{movie.title}</p>
+				{data.map(movie => (
+					<p
+						key={movie.episode_id}
+						onClick={() => {
+							setTitle(movie.title);
+							setDate(movie.release_date);
+							setDirector(movie.director);
+							setAbstract(movie.opening_crawl);
+						}}>
+						{movie.title}
+					</p>
 				))}
 				<span className='navTitles'>FAVORITES</span>
 			</div>
+			<div className='content'>
+				{loading && <span className='loadingTxt'>{loadingMsg}</span>}
+				{error && <span>'OOOps! a wookiee got in the way!'</span>}
 
-			<div className='content'>CONTENT</div>
+				<div className='filmInfo'>
+					<span className='filmDetails'>Title: {title}</span>
+					<span className='filmDetails'>Director: {director}</span>
+					<span className='filmDetails'>Date: {date}</span>
+				</div>
+
+				<div className='abstract'>{abstract}</div>
+			</div>
 		</div>
 	);
 };
